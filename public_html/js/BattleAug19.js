3cd2e84cc005f5cda1ce032a8c67cdbd18e509fd
Tim Donlan <timdonlan@gmail.com>
2013-08-20 14:53:09 -0400

    Added Item and Special Abilities

--- public_html/js/BattleAug19.js
+++ public_html/js/BattleAug19.js
@@ -1,15 +1,17 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 var battleState = 0;
 
 var playerJSON = {"name": "Player", "TotalHP": 100, "CurrentHP": 100, "AttackScore": 50, "DefenseScore": 50, "WeaponDmg": 10};
 var enemyJSON = {"name": "Goblin", "TotalHP": 50, "CurrentHP": 50, "AttackScore": 10, "DefenseScore": 10, "WeaponDmg": 5};
 
+var playerItemJSON = {"Items": [{"name": "Healing Potion", "type": "Heal", "amount": 10},{"name": "Healing Potion", "type": "Heal", "amount": 10}]};
+var playerAbilitiesJSON = {"Abilities": [{"name": "Super Attack", "type":"damage", "cooldown": 2, "amount": 25}]};
+
+var enemyItemJSON = {"Items": [{"name": "Healing Potion"}, {"name": "Rusty Dagger"}, {"name": "Copper Key"}]};
+
 var player;
+var playerItems;
+var playerAbilities;
 var enemy;
 
 var combatStatus = "";
@@ -22,7 +24,11 @@ var logID;
 function initBattle(battleID, logID)
 {
     player = playerJSON;
+    playerItems = playerItemJSON;
+    playerAbilities = playerAbilitiesJSON;
+
     enemy = enemyJSON;
+
 
     battleState = 0;
     this.battleID = battleID;
@@ -33,7 +39,6 @@ function initBattle(battleID, logID)
         BattleLoop();
     }, 1000);
 
-
     UpdateDisplay();
 
 }
@@ -42,8 +47,6 @@ function UpdateDisplay()
 {
     var battleElement = document.getElementById(battleID);
     battleElement.innerHTML = printBattle();
-
-
     var battleElement = document.getElementById(logID);
     battleElement.innerHTML = combatStatus;
 }
@@ -51,7 +54,7 @@ function UpdateDisplay()
 function BattleLoop()
 {
 
-    UpdateDisplay()
+    UpdateDisplay();
 
     switch (battleState)
     {
@@ -78,15 +81,29 @@ function BattleLoop()
 
 }
 
-function playerHeal()
+function playerHeal(amount)
 {
-    player.CurrentHP += 10;
-    if(player.CurrentHP > player.TotalHP)
-        {
-            player.CurrentHP = player.TotalHP;
-        }
-    combatStatus += player.name + " healed for 10. <br>";
+    player.CurrentHP += amount;
+    if (player.CurrentHP > player.TotalHP)
+    {
+        player.CurrentHP = player.TotalHP;
+    }
+    combatStatus += player.name + " healed for " + amount + "<br>";
     battleState = 2;
+}
+
+function playerDamageEnemy(amount)
+{
+    enemy.CurrentHP = enemy.CurrentHP - amount;
+    combatStatus += player.name + " damaged " + enemy.name + " for " + amount + " damage" + "<br>";
+    if (enemy.CurrentHP <= 0)
+    {
+        WinBattle();
+    }
+    else
+    {
+        battleState = 2;
+    }
 }
 
 function playerAttack()
@@ -115,9 +132,7 @@ function playerAttack()
     {
         battleState = 2;
     }
-
 }
-
 
 function enemyAttack()
 {
@@ -145,7 +160,6 @@ function enemyAttack()
     {
         battleState = 1;
     }
-
 
 }
 
@@ -212,19 +226,113 @@ function printBattle()
     htmlStr += "<tr><td>Weapon Dmg:</td><td>" + player.WeaponDmg + "</td><td>" + enemy.WeaponDmg + "</td></tr>";
     htmlStr += "</table>";
 
+
     if (battleState === 1)
     {
         htmlStr += "<br><input type='button' onclick='playerAttack();'>Attack!</input>";
-         htmlStr += "<br><input type='button' onclick='playerHeal();'>Heal!</input>";
+        //htmlStr += "<br><input type='button' onclick='playerHeal();'>Heal!</input>";
     }
     else
     {
         htmlStr += "<br><input type='button' onclick='playerAttack();' disabled>Attack!</input>";
-         htmlStr += "<br><input type='button' onclick='playerAttack();' disabled>Heal!</input>";
+        //htmlStr += "<br><input type='button' onclick='playerHeal();' disabled>Heal!</input>";
     }
+     htmlStr += DisplayAbilities();
+    htmlStr += DisplayItems();
 
     return htmlStr;
 
 }
 
 
+
+function UseItem(itemName)
+{
+    //get the item from the item list
+    var usedItem;
+
+    for (var item in playerItems.Items)
+    {
+        if (playerItems.Items[item].name === itemName)
+        {
+            usedItem = playerItems.Items[item];
+        }
+    }
+    
+    combatStatus += player.name + " used " + usedItem.name + "<br>";
+
+    if (usedItem.type === "heal")
+    {
+        playerHeal(usedItem.amount);
+    }
+    if (usedItem.type === "damage")
+    {
+        playerDamageEnemy(usedItem.amount);
+    }
+
+    //remove used item
+    var index = playerItems.Items.indexOf(usedItem);
+    playerItems.Items.splice(index, 1);
+    battleState = 2;
+
+}
+
+function UseAbility(abName)
+{
+     //get the item from the item list
+    var usedAbility;
+
+    for (var ab in playerAbilities.Abilities)
+    {
+        if (playerAbilities.Abilities[ab].name === abName)
+        {
+            usedAbility = playerAbilities.Abilities[ab];
+        }
+    }
+    
+    combatStatus += player.name + " performed " + usedAbility.name + "<br>";
+
+    if (usedAbility.type === "heal")
+    {
+        playerHeal(usedAbility.amount);
+         battleState = 2;
+    }
+    if (usedAbility.type === "damage")
+    {
+        playerDamageEnemy(usedAbility.amount);
+    }
+
+    //remove used item
+    //var index = playerAbilities.Abilities.indexOf(usedItem);
+    //playerAbilities.Abilities.splice(index, 1);
+   
+    
+}
+
+function DisplayAbilities()
+{
+     var retval = "";
+    for (var ab in playerAbilities.Abilities)
+    {
+        var functionCall = "UseAbility('" + playerAbilities.Abilities[ab].name + "');";
+       
+        retval += "<br><input type='button' onclick=\""+functionCall+"\" >" + playerAbilities.Abilities[ab].name + "</input>";
+          
+    }
+
+    return retval;
+}
+
+function DisplayItems()
+{
+    var retval = "";
+    for (var item in playerItems.Items)
+    {
+        var functionCall = "UseItem('" + playerItems.Items[item].name + "');";
+       
+        retval += "<br><input type='button' onclick=\""+functionCall+"\" >" + playerItems.Items[item].name + "</input>";
+          
+    }
+
+    return retval;
+}
